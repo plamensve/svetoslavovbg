cf96725c069c341771cf23f09cc3ab8e49b8daa7
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-04-17 23:14:26 +0300

    ADD: New design

---
+++ recources/js/full-width.js
@@ -0,0 +1,27 @@
+const section = document.querySelector('.first-section');
+const mainHeader = document.querySelector('.main-header')
+const topHeader = document.querySelector('.top-lane-contact')
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            section.classList.add('full-width');
+            section.style.borderRadius = '0px';
+            mainHeader.style.background = "#3a7a7b";
+            mainHeader.style.backgroundSize = "100% 200%";
+            mainHeader.style.backgroundPosition = "top";
+            mainHeader.style.boxShadow = "";
+
+
+        } else {
+            section.classList.remove('full-width');
+            section.style.borderRadius = '30px';
+            mainHeader.style.background = 'linear-gradient(to right, #479698, #2b4d4e)';
+        }
+    });
+}, {
+    threshold: 0.59
+});
+
+
+observer.observe(section);
