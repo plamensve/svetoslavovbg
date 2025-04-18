ab429cda514ea761cbf8e88b4d20fb389548fd70
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-04-18 08:00:06 +0300

    ADD: New design

--- recources/js/full-width.js
+++ recources/js/full-width.js
@@ -7,18 +7,14 @@ const observer = new IntersectionObserver(entries => {
         if (entry.isIntersecting) {
             section.classList.add('full-width');
             section.style.borderRadius = '0px';
-            mainHeader.style.background = "#3a7a7b";
-            mainHeader.style.backgroundSize = "100% 200%";
-            mainHeader.style.backgroundPosition = "top";
-            mainHeader.style.boxShadow = "";
+
         } else {
             section.classList.remove('full-width');
             section.style.borderRadius = '30px';
-            mainHeader.style.background = 'linear-gradient(to right, #479698, #2b4d4e)';
         }
     });
 }, {
-    threshold: 0.55
+    threshold: 0.56
 });
 
 
