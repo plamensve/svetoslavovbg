f161f62042a574b61eaf7fc374f73b6315d826e5
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2025-05-02 01:06:05 +0300

    ADD: 02.05.2025 - 01:00h

--- recources/js/gallery.js
+++ recources/js/gallery.js
@@ -1,14 +1,24 @@
 let photos = document.querySelectorAll('.gallery-item');
 
 photos.forEach(photo => {
+    let isZoomed = false;
+
     photo.addEventListener('click', function () {
-        photo.style.transform = 'scale(2)';
-        photo.style.zIndex = '10';
-        photo.style.transition = 'transform 0.3s ease';
+        if (!isZoomed) {
+            photo.style.transform = 'scale(2)';
+            photo.style.zIndex = '10';
+        } else {
+            photo.style.transform = 'scale(1)';
+            photo.style.zIndex = '1';
+        }
+        isZoomed = !isZoomed;
     });
 
     photo.addEventListener('mouseleave', function () {
-        photo.style.transform = 'scale(1)';
-        photo.style.zIndex = '1';
+        if (isZoomed) {
+            photo.style.transform = 'scale(1)';
+            photo.style.zIndex = '1';
+            isZoomed = false;
+        }
     });
 });
